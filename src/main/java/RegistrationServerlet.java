078e968ba855bab1a447a7e8c6bc8090d60eb59a
KaveeshaLakshan2 <kaveesha2002lakshan@.com>
2025-05-23 21:56:06 +0530

    login

---
+++ src/main/java/RegistrationServerlet.java
@@ -0,0 +1,64 @@
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+
+
+/**
+ * Servlet implementation class RegistrationServerlet
+ */
+@WebServlet("/register")
+public class RegistrationServerlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		String uname = request.getParameter("name");
+		String uemail = request.getParameter("email");
+		String upwd = request.getParameter("pass");
+		String umobile = request.getParameter("contact");
+		jakarta.servlet.RequestDispatcher dispatcher = null;
+		Connection con = null;
+	
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			con = DriverManager.getConnection("jdbc:mysql://localhost:3306/signup","root","1234");
+			PreparedStatement pst = con.prepareStatement("insert into users(uname,upwd,uemail,umobile) values(?,?,?,?)");
+			pst.setString(1, uname);
+			pst.setString(2, upwd);
+			pst.setString(3, uemail);
+			pst.setString(4, umobile);
+			
+			int rowCount = pst.executeUpdate();
+			dispatcher = request.getRequestDispatcher("registration.jsp");
+			if(rowCount > 0) {
+				request.setAttribute("status","success");
+			}else {
+				request.setAttribute("status","failed");
+			}
+			
+			dispatcher.forward(request, response);
+			
+			
+		}catch (Exception e){
+			e.printStackTrace();
+		}finally {
+			try {
+				con.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		
+	}
+
+}
